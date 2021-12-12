d9cb5599a783be3d2d5f9040f6a6588461f5c8e8
german nunez <german90nunez@gmail.com>
2021-12-11 20:16:29 -0600

    setup and imported comps from material ui and styled comp for wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
+import { useState } from 'react'
+import { useQuery } from 'react-query'
+
+import Drawer from '@material-ui/core/Drawer'
+import LinearProgress from '@material-ui/core/LinearProgress'
+import Grid from '@material-ui/core/Grid';
+import Badge from '@material-ui/core/Badge'
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart'
+
+import { Wrapper }from './App.styles'
 
 const App = () => {
   return (
     <div className="App">
-      start
+      <AddShoppingCartIcon />
     </div>
   );
 }
