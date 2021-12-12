c3c9d9a01b51ed12b9d781422f8c010ca6496aac
german nunez <german90nunez@gmail.com>
2021-12-12 14:17:16 -0600

    mvp finshed, running total added, add and remove form cart added and some other styles

--- src/Cart/Cart.tsx
+++ src/Cart/Cart.tsx
@@ -13,6 +13,10 @@ type Props = {
 }
 
 const Cart: React.FC<Props> = ({cartItems, addToCart, removeFromCart}) => {
+    const calculateTotal = ( items: CartItemType[]) => (
+        items.reduce((ack: number, item) => ack + item.amount * item.price, 0)
+    )
+
     return ( 
         <Wrapper>
             <h2>In your cart</h2>
@@ -25,6 +29,8 @@ const Cart: React.FC<Props> = ({cartItems, addToCart, removeFromCart}) => {
                     addToCart={addToCart}
                     removeFromCart={removeFromCart} />
             ))}
+
+            <h2>Total: ${calculateTotal(cartItems).toFixed(2)}</h2>
         </Wrapper>
     )
 }
