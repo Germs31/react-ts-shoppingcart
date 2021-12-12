f49cf76afbf4ae01da2b41af6e62032a9ba0ea7f
german nunez <german90nunez@gmail.com>
2021-12-11 20:32:37 -0600

    fetching data from fake store api, react query is pretty cool

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,32 @@ import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart'
 
 import { Wrapper }from './App.styles'
 
+export type CartItemType = {
+  id: number;
+  catergory: string;
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+  amount: number;
+}
+
+const getProducts = async (): Promise<CartItemType[]> => {
+  return await (await fetch('https://fakestoreapi.com/products')).json()
+}
 const App = () => {
+  const { data, isLoading,  error } = useQuery<CartItemType[]>('products',getProducts)
+
+  console.log(data)
+  const getTotalItems = () => null 
+  const handleAddToCart = () => null
+  const handleRemoveFromCart = () => null
+
+  if(isLoading) return <LinearProgress />
+  if(error) return <div>Something went wrong ...</div>
   return (
     <div className="App">
+
       <AddShoppingCartIcon />
     </div>
   );
