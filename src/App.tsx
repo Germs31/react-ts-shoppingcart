a9d00c478c0e7ebe0b5aa713ef4dd0b8f2f6a5d7
german nunez <german90nunez@gmail.com>
2021-12-12 11:18:29 -0600

    mui drawer styled button icon

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,14 @@ import { useQuery } from 'react-query'
 
 import Item from './Item/Item'
 
+
 import Drawer from '@material-ui/core/Drawer'
 import LinearProgress from '@material-ui/core/LinearProgress'
 import Grid from '@material-ui/core/Grid';
 import Badge from '@material-ui/core/Badge'
 import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart'
 
-import { Wrapper }from './App.styles'
+import { Wrapper, StyledButton }from './App.styles'
 
 export type CartItemType = {
   id: number;
@@ -25,10 +26,13 @@ const getProducts = async (): Promise<CartItemType[]> => {
   return await (await fetch('https://fakestoreapi.com/products')).json()
 }
 const App = () => {
+  const [cartOpen, setCartOpen] = useState(false)
+  const [cartItems, setCartItems] = useState([] as CartItemType[])
+
   const { data, isLoading,  error } = useQuery<CartItemType[]>('products',getProducts)
 
   console.log(data)
-  const getTotalItems = () => null 
+  const getTotalItems = ( items: CartItemType[]) => null 
   const handleAddToCart = (clickedItem : CartItemType) => null
   const handleRemoveFromCart = () => null
 
@@ -36,6 +40,16 @@ const App = () => {
   if(error) return <div>Something went wrong ...</div>
   return (
     <Wrapper>
+      <Drawer anchor='right' open={cartOpen} onClose={() => setCartOpen(false)} >
+        Cart Goes Here
+      </Drawer>
+
+      <StyledButton onClick={() => setCartOpen(true)}>
+        <Badge badgeContent={getTotalItems(cartItems)} color='error'>
+          <AddShoppingCartIcon />
+        </Badge>
+      </StyledButton>
+
       <Grid container spacing={3}>
         {data?.map(item => (
           <Grid item key={item.id} xs={12} sm={4}>
