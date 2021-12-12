4a9394faa30297cb5fb3217610dfe10f5d59f7fa
german nunez <german90nunez@gmail.com>
2021-12-12 13:44:14 -0600

    item getting added to cart, update item amount added

--- src/CartItem/CartItem.tsx
+++ src/CartItem/CartItem.tsx
@@ -1,10 +1,45 @@
-import Buttom from '@material-ui/core/Button'
+import Button from '@material-ui/core/Button'
 import React from 'react'
 
 import {CartItemType} from '../App'
+import Item from '../Item/Item'
 
 import { Wrapper } from './CartItem.styles'
 
-const CartItem: React.FC = () => <div>Cart ITEM</div>
+type Props = {
+     item: CartItemType;
+     addToCart: (clickedItem: CartItemType) => void;
+     removeFromCart: (id: number) => void;
+}
+
+const CartItem: React.FC<Props> = ({item, addToCart, removeFromCart}) =>(
+    <Wrapper>
+        <div>
+            <h3>{item.title}</h3>
+            <div className="information">
+                <p>Price ${item.price}</p>
+                <p>Total: ${(item.amount * item.price).toFixed(2)}</p>
+            </div>
+            <div className="buttons">
+                <Button 
+                    size="small" 
+                    disableElevation 
+                    variant="contained"
+                    onClick={() => removeFromCart(item.id)}>
+                        -
+                </Button>
+                    <p>{item.amount}</p>
+                <Button 
+                    size="small" 
+                    disableElevation 
+                    variant="contained"
+                    onClick={() => addToCart(item)}>
+                        +
+                </Button>
+            </div>
+            <img src={item.image}  alt={item.title}/>      
+        </div>
+    </Wrapper>
+)
 
 export default CartItem
